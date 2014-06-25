f7942994824d1245947efb03f30a3e2756aa41e2
andrewlxia <andrewlxia@gmail.com>
2014-06-25 16:21:18 -0700

    Create Balanced_Binary_Tree.java

---
+++ Balanced_Binary_Tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(getHeight(root) == -1) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (left == -1 || right == -1 || Math.abs(left-right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
