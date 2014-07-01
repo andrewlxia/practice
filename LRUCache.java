41bbd9f9a9017ce8d8956fbd1f376d3900c1f07b
andrewlxia <andrewlxia@gmail.com>
2014-07-01 14:13:33 -0700

    Update LRUCache.java

--- LRUCache.java
+++ LRUCache.java
@@ -2,6 +2,8 @@ public class LRUCache {
     public class cacheNode {
         int key;
         int val;
+        cacheNode prev;
+        cacheNode next;
         
         public cacheNode(int key, int val) {
             this.key = key;
@@ -9,20 +11,23 @@ public class LRUCache {
         }
     }
     private Map <Integer, cacheNode> cache;
-    private LinkedList <cacheNode> list;
+    private cacheNode head, tail; //dummy nodes
     private int capacity;
     
     public LRUCache(int capacity) {
         this.capacity = capacity;
         cache = new HashMap<> ();
-        list = new LinkedList<> ();
+        head = new cacheNode(0, 0);
+        tail = new cacheNode(0, 0);
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
         cacheNode node = cache.get(key);
         if (node != null) {
-            list.remove(node);
-            list.addFirst(node);
+            remove(node);
+            addFirst(node);
             return node.val;
         }
         
@@ -33,16 +38,42 @@ public class LRUCache {
         cacheNode node = cache.get(key);
         if (node != null) {
             node.val = value;
-            list.remove(node);
-            list.addFirst(node);
+            remove(node);
+            addFirst(node);
         } else {
             node = new cacheNode(key, value);
             if (cache.size() >= capacity) {
-                cacheNode nodeToRemove = list.removeLast();
-                cache.remove(nodeToRemove.key);
+                cacheNode nodeToRemove = removeLast();
+                if (nodeToRemove != null) {
+                    cache.remove(nodeToRemove.key);
+                }
             }
             cache.put(key, node);
-            list.addFirst(node);
+            addFirst(node);
         }
+    }
+    
+    private void remove(cacheNode node) {
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+        node.next = node.prev = null;
+    }
+    
+    private void addFirst(cacheNode node) {
+        node.next = head.next;
+        node.prev = head;
+        node.next.prev = node;
+        head.next = node;
+    }
+    
+    private cacheNode removeLast() {
+        if (tail.prev == head) {
+            return null;
+        }
+        cacheNode node = tail.prev;
+        tail.prev = node.prev;
+        tail.prev.next = tail;
+        
+        return node;
     }
 }
